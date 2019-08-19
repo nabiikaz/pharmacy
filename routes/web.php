7cdec24255501ca1aae9f30a9a426f4b680b76c2
nabiikaz <nabiikaz2017@gmail.com>
2019-08-19 14:08:05 +0100

    laratrust.php config file modified ->handle changed from abort to redirect web.php route file modifed ->dashboard prefix created and middleware role check

--- routes/web.php
+++ routes/web.php
@@ -11,65 +11,69 @@
 |
 */
 
-Route::get('dashboard', function () {
-    return view('layouts.dashboard');
+Route::prefix('dashboard')->middleware("role:superadministrator|administrator")->group(function(){
+    Route::get('/',function(){
+        return view('layouts.dashboard');
+    });
+
+    
+
+    Route::get('users',function(){
+        return view('users');
+    });
+
+
+    Route::get('medicines',function(){
+        return view('medicines');
+    });
+
+    //batches --------------------------------------------------------
+    Route::get('medicines/batches/{id}',function($id){
+        return view('batches',compact('id'));
+    });
+
+    Route::get('medicines/batches',function(){
+        $id= -1;
+        return view('batches')->with('id',$id);
+    });
+
+    //sales --------------------------------------------------
+    Route::get('medicines/sales',function(){
+        $id= -1;    
+        return view('sales')->with('id',$id);
+    });
+
+
+    Route::get('medicines/sales/{id}',function($id){
+        return view('sales')->with('id',$id);
+    });
+
+    //suppliers  --------------------------------------------------
+
+    Route::get('suppliers',function(){
+        $id= -1;    
+        return view('suppliers')->with('id',$id);
+    });
+
+
+    Route::get('suppliers/{id}',function($id){
+        return view('suppliers')->with('id',$id);
+    });
+
+
+    //customers  --------------------------------------------------
+
+    Route::get('customers',function(){
+        $id= -1;    
+        return view('customers')->with('id',$id);
+    });
+
+
+    Route::get('customers/{id}',function($id){
+        return view('customers')->with('id',$id);
+    });
 });
 
-
-Route::get('dashboard/users',function(){
-    return view('users');
-});
-
-
-Route::get('dashboard/medicines',function(){
-    return view('medicines');
-});
-
-//batches --------------------------------------------------------
-Route::get('dashboard/medicines/batches/{id}',function($id){
-    return view('batches',compact('id'));
-});
-
-Route::get('dashboard/medicines/batches',function(){
-    $id= -1;
-    return view('batches')->with('id',$id);
-});
-
-//sales --------------------------------------------------
-Route::get('dashboard/medicines/sales',function(){
-    $id= -1;    
-    return view('sales')->with('id',$id);
-});
-
-
-Route::get('dashboard/medicines/sales/{id}',function($id){
-    return view('sales')->with('id',$id);
-});
-
-//suppliers  --------------------------------------------------
-
-Route::get('dashboard/suppliers',function(){
-    $id= -1;    
-    return view('suppliers')->with('id',$id);
-});
-
-
-Route::get('dashboard/suppliers/{id}',function($id){
-    return view('suppliers')->with('id',$id);
-});
-
-
-//customers  --------------------------------------------------
-
-Route::get('dashboard/customers',function(){
-    $id= -1;    
-    return view('customers')->with('id',$id);
-});
-
-
-Route::get('dashboard/customers/{id}',function($id){
-    return view('customers')->with('id',$id);
-});
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
