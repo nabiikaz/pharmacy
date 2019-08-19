9bb3622c926755d67c282cc2bea6895c12130a0a
nabiikaz <nabiikaz2017@gmail.com>
2019-08-19 13:49:18 +0100

    laratrust installed and configured

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,8 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 
         $this->call(MedicinesTableSeeder::class);
+
+
+        $this->call(LaratrustSeeder::class);
     }
 }
